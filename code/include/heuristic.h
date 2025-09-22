94106eecdefc89500e71337511dbe51cee735e88
Jeffrey Ke <jeffke613@gmail.com>
2025-09-21 22:29:02 -0400

    Bugfixes, and making sure heuristic returns 0 for the imaginary goal If heuristic has detected the imaginary goal, it returns 0

--- code/include/heuristic.h
+++ code/include/heuristic.h
@@ -24,9 +24,12 @@ struct DistanceHeuristic: public Heuristic {
 	{};
 
 	int operator() (const State& state) const override {
+		if (state == Map::IMAGINARY_GOAL) {
+			return 0;
+		}
 		const auto& [cur_x, cur_y, cur_t] = state;
-		const auto cur_t_clamped = std::max<int>(cur_t, _target.size() - 1);
-		const auto& [tar_x, tar_y, tar_t] = _target.at(cur_t);  //is it guaranteed that _target will be at least steps long? return std::sqrt(
+		const auto cur_t_clamped = std::min<int>(cur_t, _target.size() - 1);
+		const auto& [tar_x, tar_y, tar_t] = _target.at(cur_t_clamped);  //is it guaranteed that _target will be at least steps long? return std::sqrt(
 		return std::sqrt(
 			std::pow(cur_x - tar_x, 2) +
 			std::pow(cur_y - tar_y, 2)
