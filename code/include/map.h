3c91cacd21db83b141331715a30e01ea4a8df1b9
Jeffrey Ke <jeffke613@gmail.com>
2025-09-26 14:00:00 -0400

    Big optimizations for hash computation

--- code/include/map.h
+++ code/include/map.h
@@ -8,12 +8,13 @@
 
 #include "planner.h"
 #include "state.h"
+using std::array;
 using std::vector;
 using std::unordered_map;
 using std::unordered_set;
 
 struct Map {
-	static const vector<Action> ACTIONS;
+	static const array<Action, 9> ACTIONS;
 	static const State IMAGINARY_GOAL;
 	static constexpr double BIG_GVAL = std::numeric_limits<double>::infinity();
 
@@ -35,27 +36,22 @@ struct Map {
 			_concrete_goals.insert(goal);
 
 		}
+		_legal_states.reserve(ACTIONS.size() + 1);
 	};
 
 	void set_start(const State& state) {
 		update_gval(state, 0.0);
 	};
 	void update_gval(const State& state, gval val) {
-		auto state_iter = _state_gval_table.find(state);
-		if (state_iter == _state_gval_table.end()) {
-			_state_gval_table[state] = val;
-		}
-		else if (const auto& [s, cur_gval] = *state_iter; val < cur_gval) {
-			_state_gval_table.at(s) = val;
+		auto [iter, inserted] = _state_gval_table.try_emplace(state, val);
+		if (!inserted and val < iter->second) {
+			iter->second = val;
 		}
 	};
 
 	const gval get_gval(const State& state) const {
-		if (_state_gval_table.find(state) != _state_gval_table.end())
-			return _state_gval_table.at(state);
-		else {
-			return std::numeric_limits<double>::infinity();
-		}
+		auto it = _state_gval_table.find(state);
+		return (it != _state_gval_table.end()) ? it->second : std::numeric_limits<double>::infinity();
 	};
 
 	bool is_valid(const State& s) const {
@@ -70,31 +66,32 @@ struct Map {
 	};
 	using Gval = double;
 	using StateGvalPair = std::pair<State, Gval>;
-	const vector<StateGvalPair> operator[] (const State& cur)  {
-		vector<StateGvalPair> legal_states;
+	mutable vector<StateGvalPair> _legal_states;
+	const vector<StateGvalPair>& operator[] (const State& cur)  {
+		_legal_states.clear();
+		auto cur_gval = get_gval(cur);
 		for (const auto& action : ACTIONS) {
 			State next_state = cur + action;
 			if (is_valid(next_state)) {
-				auto cur_gval = get_gval(cur); //of current! not of next state
 				auto cost = static_cast<double>(
 					_raw[GETMAPINDEX(next_state.x, next_state.y, _x_size, _y_size)]
 				);
 				update_gval(next_state, cur_gval + cost);
-				legal_states.push_back({next_state, cur_gval + cost});
+				_legal_states.push_back({next_state, cur_gval + cost});
 			}
 		}
 		if (_concrete_goals.find(cur) != _concrete_goals.end()) {
-			auto cur_gval = get_gval(cur);
 			update_gval(IMAGINARY_GOAL, cur_gval + 1);
-			legal_states.push_back(
+			_legal_states.push_back(
 				{IMAGINARY_GOAL, cur_gval + 1}
 			);
 		}
-		return legal_states;
+		return _legal_states;
 	};
-	const vector<StateGvalPair> backwards(const State& s) {
-		vector<StateGvalPair> backwards_preds;
-		const vector<Action> actions = {
+	mutable vector<StateGvalPair> backwards_preds;
+	const vector<StateGvalPair>& backwards(const State& s) {
+		backwards_preds.clear();
+		const array<Action, 9> actions = {
 			Action{-1, -1, -1},
 			Action{-1, 0, -1},
 			Action{-1, 1, -1},
@@ -128,7 +125,7 @@ struct Map {
 	}
 };
 inline const State Map::IMAGINARY_GOAL{0, 0, 0};
-inline const vector<Action> Map::ACTIONS = {
+inline const array<Action, 9> Map::ACTIONS = {
 	Action{-1, -1, 1},
 	Action{-1, 0, 1},
 	Action{-1, 1, 1},
