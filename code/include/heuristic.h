0474a20223ad187eb44fbc606c8d9ec7bd11b5d4
Jeffrey Ke <jeffke613@gmail.com>
2025-09-26 11:37:27 -0400

    Added octile heuristic with "lookahead": how far in the target's trajectory to look ahead. Also made DistanceHeuristic more like octile heuristic in its implementation.

--- code/include/heuristic.h
+++ code/include/heuristic.h
@@ -26,12 +26,35 @@ struct DistanceHeuristic: public Heuristic {
 			return 0;
 		}
 		const auto& [cur_x, cur_y, cur_t] = state;
-		const auto cur_t_clamped = std::min<int>(cur_t, _target.size() - 1);
-		const auto& [tar_x, tar_y, tar_t] = _target.at(cur_t_clamped);  //is it guaranteed that _target will be at least steps long? return std::sqrt(
+		const auto& [tar_x, tar_y, tar_t] = _target.at(cur_t);  //is it guaranteed that _target will be at least steps long? return std::sqrt(
+		double dx = std::abs(cur_x - tar_x);
+		double dy = std::abs(cur_y - tar_y);
 		return _weight * std::sqrt(
-			std::pow(cur_x - tar_x, 2) +
-			std::pow(cur_y - tar_y, 2)
+			std::pow(dx, 2) +
+			std::pow(dy, 2)
 		);
 	};
+};
+struct OctileHeuristic: public Heuristic {
+	const vector<State>& _target;
+	double _weight;
+	int _look_ahead;
+
+	OctileHeuristic(const vector<State>& target, double weight, int look_ahead): _target(target), _weight(weight), _look_ahead(look_ahead){};
+
+	double operator() (const State& state) const override {
+		if (state == Map::IMAGINARY_GOAL) {
+			return 0;
+		}
+		const auto& [cur_x, cur_y, cur_t] = state;
+		int index = std::min<int>(cur_t + _look_ahead, _target.size() - 1);
+		const auto& [tar_x, tar_y, tar_t] = _target.at(cur_t);  //is it guaranteed that _target will be at least steps long? return std::sqrt(
+		double dx = std::abs(cur_x - tar_x);
+		double dy = std::abs(cur_y - tar_y);
+		return _weight * (
+			std::sqrt(2) * std::min(dx, dy) + 
+			std::abs(dx - dy)
+		);
+	}
 };
 #endif
