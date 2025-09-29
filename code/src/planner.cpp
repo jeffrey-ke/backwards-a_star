fc0fc5797600cf31fcc379badeef5a90993dc23f
Jeffrey Ke <jeffke613@gmail.com>
2025-09-29 11:07:30 -0400

    For map2, the budget is smaller

--- code/src/planner.cpp
+++ code/src/planner.cpp
@@ -100,7 +100,7 @@ void planner(
 	State goal{robot_init};
 
 	Map map{raw_map, x_size, y_size, collision_thresh, concrete_goals, start, A_STAR_MODE};
-	DijkstraHeuristic heuristic(raw_map, x_size, y_size, collision_thresh, concrete_goals, goal, 100000, target_steps - 5);
+	DijkstraHeuristic heuristic(raw_map, x_size, y_size, collision_thresh, concrete_goals, goal, 100000, target_steps - 20);
 	// TODO: breakpoint here
 	open.insert_update(start, 0.0, heuristic);
 
