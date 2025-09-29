a4cfeef958a78112f1fd2b1d7a4ab6472135656b
Jeffrey Ke <jeffke613@gmail.com>
2025-09-28 23:02:04 -0400

    wip for steps

--- code/include/map.h
+++ code/include/map.h
@@ -26,6 +26,7 @@ struct Map {
 
 	static const State IMAGINARY_GOAL;
 	static constexpr double BIG_GVAL = std::numeric_limits<double>::infinity();
+	static constexpr int MAX_STEP = std::numeric_limits<int>::infinity();
 
 	const array<Action, 9> _actions;
 
@@ -35,6 +36,7 @@ struct Map {
 	State _start;
 	unordered_set<State, State::Hasher> _concrete_goals;
 	unordered_map<State, gval, State::Hasher> _state_gval_table;
+	unordered_map<State, int, State::Hasher> _state_steps_table;
 	mutable vector<StateGvalPair> _legal_states;
 	mutable cv::Mat _distance_map;
 
@@ -54,13 +56,13 @@ struct Map {
 		)
 	{
 
-		update_gval(start, 0.0);
+		update_gval(start, 0.0, 0);
 		for (const auto& goal: concrete_goals) {
 			_concrete_goals.insert(goal);
 			if (mode == MODE::FORWARD)
-				update_gval(goal, BIG_GVAL);
+				update_gval(goal, BIG_GVAL, MAX_STEP);
 			else if (mode == MODE::BACKWARD)
-				update_gval(goal, 1);
+				update_gval(goal, 1, 1);
 
 		}
 		_legal_states.reserve(_actions.size() + 1);
@@ -100,17 +102,25 @@ struct Map {
 		return (is_valid(state)) ? _distance_map.at<float>(state.y - 1, state.x - 1) : 0.0;
 	}
 
-	void update_gval(const State& state, gval val) {
+	void update_gval(const State& state, gval val, int dist) {
 		auto [iter, inserted] = _state_gval_table.try_emplace(state, val);
+		auto [iter2, inserted2] = _state_steps_table.try_emplace(state, dist);
+		assert(inserted == inserted2);
 		if (!inserted and val < iter->second) {
 			iter->second = val;
+			iter2->second = dist;// could be more steps, but lower cost
 		}
 	};
 
-	const gval get_gval(const State& state) const {
+	gval get_gval(const State& state) const {
 		auto it = _state_gval_table.find(state);
 		return (it != _state_gval_table.end()) ? it->second : BIG_GVAL;
 	};
+
+	int get_steps(const State& state) const {
+		auto it = _state_steps_table.find(state);
+		return (it != _state_steps_table.end()) ? it->second : MAX_STEP;
+	}
 
 	bool is_valid(const State& s) const {
 		const auto index = GETMAPINDEX(s.x, s.y, _x_size, _y_size);
@@ -136,6 +146,7 @@ struct Map {
 	const vector<StateGvalPair>& forwards_succ(const State& cur)  {
 		_legal_states.clear();
 		auto cur_gval = get_gval(cur);
+		auto cur_steps = get_steps(cur);
 		assert(cur_gval < BIG_GVAL);
 		for (const auto& action : _actions) {
 			State next_state = cur + action;
@@ -143,12 +154,12 @@ struct Map {
 				auto cost = static_cast<double>(
 					_raw[GETMAPINDEX(next_state.x, next_state.y, _x_size, _y_size)]
 				);
-				update_gval(next_state, cur_gval + cost);
+				update_gval(next_state, cur_gval + cost, cur_steps + 1);
 				_legal_states.push_back({next_state, cur_gval + cost});
 			}
 		}
 		if (_concrete_goals.find(cur) != _concrete_goals.end()) {
-			update_gval(IMAGINARY_GOAL, cur_gval + 1);
+			update_gval(IMAGINARY_GOAL, cur_gval + 1, cur_steps);
 			_legal_states.push_back(
 				{IMAGINARY_GOAL, cur_gval + 1}
 			);
@@ -159,6 +170,7 @@ struct Map {
 	const vector<StateGvalPair>& backwards_succ(const State& cur) {
 		_legal_states.clear();
 		auto cur_g = get_gval(cur);
+		auto cur_steps = get_steps(cur);
 		if (cur == IMAGINARY_GOAL) {
 			assert(cur_g == 0);
 			for (const auto& g : _concrete_goals){
@@ -175,7 +187,7 @@ struct Map {
 						_raw[GETMAPINDEX(next.x, next.y, _x_size, _y_size)]
 					);
 					auto next_g = cur_g + cost;
-					update_gval(next, next_g); //this is actually necessary, because it will
+					update_gval(next, next_g, cur_steps + 1); //this is actually necessary, because it will
 					//make states that it's just traveled a higher gvalue, but this update gvalue
 					//only changes it if it's lower
 					_legal_states.push_back({next, next_g});
